fdef662b2514126438795f5089034628d0f6add7
Bataklik <burak.mikail.aktas@outlook.com>
2026-05-20 11:59:05 +0200

    feat: :sparkles: update AddTask component to accept text and setText props for improved task input handling

--- app/components/add-task.tsx
+++ app/components/add-task.tsx
@@ -5,11 +5,18 @@ import { Button } from "./ui/button";
 
 interface AddTaskProps {
     onClickHandler: () => void;
+    text: string;
+    setText: React.Dispatch<React.SetStateAction<string>>;
 }
-export function AddTask({ onClickHandler }: AddTaskProps) {
+export function AddTask({ onClickHandler, text, setText }: AddTaskProps) {
     return (
         <Field orientation={"horizontal"} className="mb-5">
-            <Input type="text" placeholder="Add a new task..." />
+            <Input
+                type="text"
+                placeholder="Add a new task..."
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+            />
             <Button onClick={onClickHandler}>Add new task</Button>
         </Field>
     );

0f71ac94ab41891d8a37ee515f5b0baed0b5ea9f
Bataklik <burak.mikail.aktas@outlook.com>
2026-05-20 11:59:14 +0200

    feat: :sparkles: refactor Home component to integrate task management with Redux and enhance task input handling

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,7 +1,10 @@
 import { TaskTable } from "~/components/task-table";
 import type { Route } from "./+types/home";
 import { AddTask } from "~/components/add-task";
-
+import { useDispatch, useSelector } from "react-redux";
+import type { RootState } from "~/store";
+import { addTask } from "~/features/task-slice";
+import { useState } from "react";
 export function meta({}: Route.MetaArgs) {
     return [
         { title: "New Task App" },
@@ -10,13 +13,25 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+    const tasks = useSelector((state: RootState) => state.task);
+    const dispatch = useDispatch();
+    const [taskTitle, setTaskTitle] = useState<string>("");
+
     const addTaskHandler = () => {
-        console.log("ADD TASK!");
+        console.log("addTaskHandler " + taskTitle);
+
+        if (!taskTitle) return;
+        dispatch(addTask(taskTitle));
+        setTaskTitle("");
     };
     return (
         <div>
-            <AddTask onClickHandler={addTaskHandler} />
-            <TaskTable />
+            <AddTask
+                onClickHandler={addTaskHandler}
+                text={taskTitle}
+                setText={setTaskTitle}
+            />
+            <TaskTable tasks={tasks} />
         </div>
     );
 }

7616912fa45ee05fa265f083276ddadd24dfb8d3
Bataklik <burak.mikail.aktas@outlook.com>
2026-05-20 11:58:21 +0200

    feat: :sparkles: refactor addTask reducer to use prepare callback for improved state management

--- app/features/task-slice.ts
+++ app/features/task-slice.ts
@@ -1,19 +1,32 @@
-import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+import { createSlice, type PayloadAction, nanoid } from "@reduxjs/toolkit";
 import type { Task } from "~/types";
+import { useId } from "react";
 // https://redux-toolkit.js.org/introduction/why-rtk-is-redux-today
 // https://redux-toolkit.js.org/usage/usage-with-typescript#createslice
+// https://stackoverflow.com/questions/71446347/redux-toolkit-generate-and-return-an-id-return-data-from-action
 const taskSlice = createSlice({
     name: "2do",
-    initialState: [],
+    initialState: [] as Task[],
     reducers: {
-        addTask(state: Task[], action: PayloadAction<Task>) {
-            state.push({
-                id: action.payload.id,
-                title: action.payload.title,
-                completed: action.payload.completed,
-            });
+        addTask: {
+            prepare(title: string) {
+                return {
+                    payload: {
+                        id: nanoid(),
+                        title,
+                        completed: false,
+                    } as Task,
+                };
+            },
+            reducer(state: Task[], action: PayloadAction<Task>) {
+                state.push({
+                    id: action.payload.id,
+                    title: action.payload.title,
+                    completed: action.payload.completed,
+                });
+            },
         },
-        completeTask(state: Task[], action: PayloadAction<number>) {
+        completeTask(state: Task[], action: PayloadAction<string>) {
             const task = state.find((t) => t.id === action.payload);
             if (task) {
                 task.completed = !task.completed;

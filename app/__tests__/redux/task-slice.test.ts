db2106d371e925bf9b9190be3699d3087ca78059
Bataklik <burak.mikail.aktas@outlook.com>
2026-05-20 16:06:13 +0200

    test: :white_check_mark: add tests for task completion and removal in task-slice reducer

--- app/__tests__/redux/task-slice.test.ts
+++ app/__tests__/redux/task-slice.test.ts
@@ -1,11 +1,39 @@
 import { expect, test } from "vitest";
-import taskReducer, { addTask } from "~/features/task-slice";
+import taskReducer, {
+    addTask,
+    completeTask,
+    removeTask,
+} from "~/features/task-slice";
 import type { Task } from "~/types";
 
 test("Should add a task", () => {
     const initialState: Task[] = [];
 
     const result = taskReducer(initialState, addTask("Testing"));
+    expect(result.length).toBe(1);
+    expect(result[0].title).toBe("Testing");
+    expect(result[0].completed).toBe(false);
+});
+
+test("Should toggle a task", () => {
+    const initialState: Task[] = [
+        { id: "1", title: "Testing", completed: false },
+        { id: "2", title: "No Testing", completed: false },
+    ];
+
+    const result = taskReducer(initialState, completeTask("1"));
+    expect(result.length).toBe(2);
+    expect(result[0].title).toBe("Testing");
+    expect(result[0].completed).toBe(true);
+});
+
+test("Should remove a task", () => {
+    const initialState: Task[] = [
+        { id: "1", title: "Testing", completed: false },
+        { id: "2", title: "No Testing", completed: false },
+    ];
+
+    const result = taskReducer(initialState, removeTask("2"));
     expect(result.length).toBe(1);
     expect(result[0].title).toBe("Testing");
     expect(result[0].completed).toBe(false);

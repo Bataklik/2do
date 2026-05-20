cca8a3e4fb2815ddfc8f9188e811dafa74588a61
Bataklik <burak.mikail.aktas@outlook.com>
2026-05-20 15:18:26 +0200

    feat: :sparkles: add onCheckedChanged prop to TaskTable for handling task state changes

--- app/components/task-table.tsx
+++ app/components/task-table.tsx
@@ -4,16 +4,30 @@ import { TaskCell } from "./task-cell";
 
 interface TaskTableProps {
     tasks: Task[];
+    onCheckedChanged: (
+        e: boolean,
+        eventDetails: {
+            reason: "none";
+            event: Event;
+            cancel: () => void;
+            allowPropagation: () => void;
+            isCanceled: boolean;
+            isPropagationAllowed: boolean;
+            trigger: Element | undefined;
+        },
+    ) => void;
 }
-export function TaskTable({ tasks }: TaskTableProps) {
+export function TaskTable({ tasks, onCheckedChanged }: TaskTableProps) {
     return (
         <Table>
             <TableBody>
                 {tasks.map((task) => (
                     <TaskCell
                         key={task.id}
+                        taskId={task.id.toString()}
                         complete={task.completed}
                         task={task.title}
+                        onCheckedChanged={onCheckedChanged}
                     />
                 ))}
             </TableBody>

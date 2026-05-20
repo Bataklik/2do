418c601b16340ec59d3414676ed0e2bd973f2c91
Bataklik <burak.mikail.aktas@outlook.com>
2026-05-20 11:58:02 +0200

    feat: :sparkles: wrap Outlet component with Redux Provider for state management

--- app/root.tsx
+++ app/root.tsx
@@ -10,7 +10,8 @@ import {
 import type { Route } from "./+types/root";
 import "./app.css";
 import { Header } from "./components/layout/header";
-
+import { Provider } from "react-redux";
+import { store } from "./store";
 export const links: Route.LinksFunction = () => [
     { rel: "preconnect", href: "https://fonts.googleapis.com" },
     {
@@ -47,7 +48,11 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default function App() {
-    return <Outlet />;
+    return (
+        <Provider store={store}>
+            <Outlet />
+        </Provider>
+    );
 }
 
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
